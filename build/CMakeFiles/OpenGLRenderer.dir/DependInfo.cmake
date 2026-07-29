60dc17bea57e339683fb2c038fc03ba8a6263c3e
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-07-29 16:26:24 +0300

    removed some extra empty files

---
+++ build/CMakeFiles/OpenGLRenderer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kettu/Documents/Work/OpenGl/git_opengl_renderer/third_party/glad/src/glad.c" "CMakeFiles/OpenGLRenderer.dir/third_party/glad/src/glad.c.o" "gcc" "CMakeFiles/OpenGLRenderer.dir/third_party/glad/src/glad.c.o.d"
+  "/home/kettu/Documents/Work/OpenGl/git_opengl_renderer/src/main.cpp" "CMakeFiles/OpenGLRenderer.dir/src/main.cpp.o" "gcc" "CMakeFiles/OpenGLRenderer.dir/src/main.cpp.o.d"
+  "" "OpenGLRenderer" "gcc" "CMakeFiles/OpenGLRenderer.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/OpenGLRenderer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLRenderer.dir/link.d"
+  "CMakeFiles/OpenGLRenderer.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGLRenderer.dir/src/main.cpp.o.d"
+  "CMakeFiles/OpenGLRenderer.dir/third_party/glad/src/glad.c.o"
+  "CMakeFiles/OpenGLRenderer.dir/third_party/glad/src/glad.c.o.d"
+  "OpenGLRenderer"
+  "OpenGLRenderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGLRenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
