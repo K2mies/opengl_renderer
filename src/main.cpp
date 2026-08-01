7e903c2a97aa89367392ab0a855e155f94677520
K2Mies <rhvidste@student.hive.fi>
2026-08-01 16:12:21 +0300

    Drawing a rectangle with an EBO

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,86 @@
 
 #include <iostream>
 
-//------------------------------------------------------- Forward declarations
+//---------------------------------------------------------- Forward declarations
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
-//---------------------------------------------------------------- Vertex data
+//------------------------------------------------------------------- Vertex data
 
-//Normalized Device Coordinates (NDC)
-float vertices[] = {
-     -.5f,  -.5f,  0.0f, // Bottom left
-     0.5f,  -.5f,  0.0f, // Bottom right
-     0.0f,  0.5f,  0.0f  // Top
+//Normalized Device Coordinates (NDC) for a triangle with RGB values
+float triangle_vertices[] = {
+      -.5f,  -.5f,  0.0f,  1,  0,  0,   // Bottom left
+      0.5f,  -.5f,  0.0f,  0, 1, 0,   // Bottom right
+     0.0f, 0.5f, 0.0f, 0, 0, 1    // Top
 };
 
-//-------------------------------------------------------------------- Shaders
+//Unique vertex positions (so they do not duplicate over each other)
+float rectangle_vertices[] = {
+    0.5f,  0.5f,   0.0f,  // top right
+    0.5f,  -.5f,   0.0f,  // bottom right
+    -.5f,  -.5f,   0.0f,  // bottom left
+    -.5f,  0.5f,  0.0f   // top left 
+};
+
+//indices for index drawing
+unsigned int rectangle_indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+};
+
+//----------------------------------------------------------------------- Shaders
+
+////Vertex Shader written in GLSL and stored as a c string litteral...
+//const char *vertexShaderSource = 
+//  "#version 330 core\n"
+//  "layout (location = 0) in vec3 aPos;\n"
+//  "layout (location = 1) in vec3 aColor;\n"
+//
+//  "out vec3 ourColor;\n"
+//
+//  "void main()\n"
+//  "{\n"
+//  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//  "   ourColor = aColor;\n"
+//  "}\0";
+//
+////Fragment Shader written in GLSL and stored as a c string litteral...
+//const char *fragmentShaderSource =
+//  "#version 330 core\n"
+//  "in  vec3 ourColor;\n"
+//  "out vec4 FragColor;\n"
+//  "void main()\n"
+//  "{\n"
+//  " FragColor = vec4(ourColor, 1.0);\n"
+//  "}\n";
 
 //Vertex Shader written in GLSL and stored as a c string litteral...
 const char *vertexShaderSource = 
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+  "#version 330 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+
+  "void main()\n"
+  "{\n"
+  "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  "}\n";
 
 //Fragment Shader written in GLSL and stored as a c string litteral...
 const char *fragmentShaderSource =
-  "#version 330 core\n"
-  "out vec4 FragColor;\n"
-  "void main()\n"
-  "{\n"
-  " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-  "}\n";
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n";
 
-//----------------------------------------------------------------------- Main
+//-------------------------------------------------------------------------- Main
+
 int main (){
   
   std::cout << "Starting OpenGL Renderer..." << std::endl;
   
-  //------------------------------------------------------- 1. Initialize GLFW
+  //---------------------------------------------------------- 1. Initialize GLFW
+
   if (!glfwInit()){
     std::cerr << "Failed to initialize GLFW\n";
     return -1;
@@ -56,7 +97,7 @@ int main (){
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   #endif
   
-  //--------------------------------------------------------- 2. Create Window
+  //------------------------------------------------------------ 2. Create Window
   
   GLFWwindow* window = glfwCreateWindow(
    800,
@@ -77,12 +118,12 @@ int main (){
   // Register resize callback
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  //------------------------------------------- 3. Make OpenGL Context Current
+  //---------------------------------------------- 3. Make OpenGL Context Current
   
   //make the window context active on the current thread
   glfwMakeContextCurrent(window);
   
-  //------------------------------------------------------- 4. Initialize GLAD 
+  //---------------------------------------------------------- 4. Initialize GLAD 
   
   //Load the openGL function pointers with GLAD..
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -92,7 +133,7 @@ int main (){
       return -1;
   }
 
-  //------------------------------------------------- 5. Create OpenGL Objects 
+  //---------------------------------------------------- 5. Create OpenGL Objects 
   
   //VAO "Vertex Array Object"
   unsigned int VAO;
@@ -108,24 +149,63 @@ int main (){
   // Upload vertex data to the VBO object
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER,
-                 sizeof(vertices),
-                 vertices,
+                 sizeof(rectangle_vertices),
+                 rectangle_vertices,
                  GL_STATIC_DRAW);
 
-  // Tell OpenGL how the vertex data is laid out
+  //EBO "Element Buffer Oject"
+  unsigned int EBO;
+  glGenBuffers(1, &EBO);
+
+  // Upload vertex data to the EBO object
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(
+      GL_ELEMENT_ARRAY_BUFFER,
+      sizeof(rectangle_indices),
+      rectangle_indices,
+      GL_STATIC_DRAW
+  );
+
+  // Tell OpenGL how the vertex data is laid out which will be used by the binded VAO
+ 
+  //// First attribute (location): 
+  //glVertexAttribPointer(
+  //  0,                    // attribute location
+  //  3,                     // 3 values per vertex
+  //  GL_FLOAT,              // each value is a float
+  //  GL_FALSE,        // don't normalize
+  //  6 * sizeof(float),   // size of one vertex
+  //  (void*)0            // starts at beginning
+  //);
+
+  //// Second attribute (color):
+  //glVertexAttribPointer(
+  //  1,
+  //  3,
+  //  GL_FLOAT,
+  //  GL_FALSE,
+  //  6 * sizeof(float),
+  //  (void*)(3 * sizeof(float))
+  //);
+  //
+  //// Enable attributes location 0 and location 1
+  //glEnableVertexAttribArray(0);
+  //glEnableVertexAttribArray(1);
+
+  // VAO setting for rectangle
   glVertexAttribPointer(
-        0,                    // attribute location
-        3,                     // 3 values per vertex
-        GL_FLOAT,              // each value is a float
-        GL_FALSE,        // don't normalize
-        3 * sizeof(float),   // size of one vertex
-        (void*)0            // starts at beginning
+    0,                    // attribute location
+    3,                     // 3 values per vertex
+    GL_FLOAT,              // each value is a float
+    GL_FALSE,        // don't normalize
+    3 * sizeof(float),   // size of one vertex
+    (void*)0            // starts at beginning
   );
   
-  // Enable attribute location 0
+  // enable attribute 0
   glEnableVertexAttribArray(0);
 
-  //-------------------------------------------------- 6. Create Vertex Shader 
+  //----------------------------------------------------- 6. Create Vertex Shader 
   
   // Create an empty shader object with an id (vertexShader itself is the id ie: 15)
   unsigned int vertexShader;
@@ -148,7 +228,7 @@ int main (){
       std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
 
-  //------------------------------------------------ 7. Create Fragment Shader 
+  //--------------------------------------------------- 7. Create Fragment Shader
 
   // Create an empty shader object with an id (FragmentShader itself is the id ie: 15)
   unsigned int fragmentShader;
@@ -169,7 +249,7 @@ int main (){
       std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
 
-  //------------------------------------------------- 8. Create Shader Program
+  //---------------------------------------------------- 8. Create Shader Program
   
   //same again that the shaderProgram is an id so ie:15.
   unsigned int shaderProgram;
@@ -188,17 +268,17 @@ int main (){
       std::cout << "ERROR::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
 
-  //------------------------------------------------- 9. Delete Shader Objects
+  //---------------------------------------------------- 9. Delete Shader Objects
  
   //think of it like deleting .o files after linking is complete.
   glDeleteShader  (vertexShader);
   glDeleteShader  (fragmentShader);
 
-  //--------------------------------------------------- 10. Use Shader Program
+  //------------------------------------------------------ 10. Use Shader Program
   
   glUseProgram    (shaderProgram);
 
-  //----------------------------------------------------- 11. Set the viewport 
+  //-------------------------------------------------------- 11. Set the viewport 
   
   //glViewport(0, 0, 800, 600);
   int fbWidth, fbHeight;
@@ -206,7 +286,7 @@ int main (){
   glViewport(0, 0, fbWidth, fbHeight);
 
 
-  //-------------------------------------------------- 12. Implamentation info
+  //----------------------------------------------------- 12. Implamentation info
  
   //print useful information about the openGL implamentation
   std::cout << "Vendor:   "
@@ -223,7 +303,7 @@ int main (){
 
   std::cout << "openGL initialized successfully\n";
   
-  //---------------------------------------------------------- 13. Render Loop
+  //------------------------------------------------------------- 13. Render Loop
   
   /* Main "render loop":
    * The glfwWindowShouldClose function checks at the start of each loop 
@@ -239,27 +319,36 @@ int main (){
   
   while (!glfwWindowShouldClose(window))
   {
-      // input
+      // Input
       processInput(window);
 
-      // rendering commands here 
+      // Rendering commands here 
       glClearColor  (0.2f, 0.3f, 0.3f, 1.0f);
       glClear       (GL_COLOR_BUFFER_BIT);
 
-      /*DRAW OUR FIRST TRIANGLE*/
+      /* DRAW OUR FIRST TRIANGLE*/
       // Use our shader program
       glUseProgram  (shaderProgram);
+
+      // Set draw mode to polygon wireframe
+      //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 
       // Bind the VAO containing our vertex configuration
       glBindVertexArray(VAO);
 
-      // Draw 3 vertices as a triangle
-      glDrawArrays  (GL_TRIANGLES, 0, 3);
+      // Draw 3 triangle_vertices as a triangle
+      //glDrawArrays  (GL_TRIANGLES, 0, 3);
 
-      // check and call events and swap the buffers / present the finished fram.
+      // Draw a Rectangle
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      glBindVertexArray(0);
+
+
+      // Swap the buffers / present the finished frame.
       glfwSwapBuffers (window);
 
-      //clear input poll events (needed for inputs)
+      // Clear input poll events (needed for inputs)
       glfwPollEvents();
   }
 
@@ -275,15 +364,20 @@ int main (){
    * front buffer so the image can be displayed without still being rendered to,
    * removing all the aforementioned artifacts.*/
 
-  //-------------------------------------------------------------- 14. Cleanup
-  
-  //clear/handle all allocated memory free, close... etc
+  //----------------------------------------------------------------- 14. Cleanup
+
+  // Destroy vertex buffer, Array and program
+  glDeleteVertexArrays  (1, &VAO);
+  glDeleteBuffers       (1, &VBO);
+  glDeleteProgram       (shaderProgram);
+
+  // Clear/handle all allocated memory free, close... etc for GLFW
   glfwTerminate(); 
 
   return 0;
 }
 
-//------------------------------------------------------- non-member functions
+//---------------------------------------------------------- non-member functions
 
 //window resize update function
 void framebuffer_size_callback  (GLFWwindow* window, int width, int height)
