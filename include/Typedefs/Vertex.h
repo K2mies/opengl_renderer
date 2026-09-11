c01c8ba5211f4f2b170e50c592e4e8d26496f448
K2Mies <rhvidste@student.hive.fi>
2026-09-11 15:20:16 +0300

    updated CMakeLists to hopefully run also on linux and maxOS, will test this first though

---
+++ include/Typedefs/Vertex.h
@@ -0,0 +1,14 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+
+#include "Typedefs/Vec2.h"
+#include "Typedefs/Vec3.h"
+
+struct  Vertex
+{
+        vec3  position;
+        vec3  normal;
+        vec2  texCoords;
+};
+
+#endif
