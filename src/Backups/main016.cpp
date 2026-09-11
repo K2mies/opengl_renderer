5ee3ba0859800b2e58c13dffd4baaa661b593903
K2Mies <rhvidste@student.hive.fi>
2026-09-11 13:20:50 +0300

    updated CMake to include assimp library

--- src/Backups/main016.cpp
+++ src/Backups/main016.cpp
@@ -27,6 +27,10 @@
 // Imports
 #include <iostream>
 
+//------------------------------------------------------------------------ Macros
+#define   NUM_OF_POINT_LIGHTS 4
+#define   NUM_OF_CUBES        10
+
 //------------------------------------------------------------------------- Enums
 enum AxisType {
     
@@ -639,7 +643,7 @@ int main (){
       
       // Point Lights
       
-      for (int i = 0; i < 4; i++)
+      for (int i = 0; i < NUM_OF_POINT_LIGHTS; i++)
       {
         float time;
               time = glfwGetTime();
@@ -709,7 +713,7 @@ int main (){
       matrix.normal = mat4::normalMatrix(matrix.model);
 
 
-      for (unsigned int i = 0; i < 10; i++)
+      for (unsigned int i = 0; i < NUM_OF_CUBES; i++)
       {
         matrix.model = mat4(1.0f);
         matrix.model = matrix.model
@@ -747,7 +751,7 @@ int main (){
       lightShader.use();
 
       glBindVertexArray(light_objs[VAO]);
-      for (int i = 0; i < 4; i++ )
+      for (int i = 0; i < NUM_OF_POINT_LIGHTS; i++ )
       {
 
         matrix.model = mat4(1.0f);
